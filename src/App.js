a4fd8f51a2bf949bf7692686d0a43a842545f58a
jujulili1 <tyulianto@gmail.com>
2020-07-07 18:42:22 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
+import Login from './Components/Login/Login';
+import Pokemons from './Components/Pokemon/Pokemons'
+import PokemonsDetails from './Components/PokemonsDetail/PokemonsDetails'
+
+import PrivateRoute from './Helpers/PrivateRoute'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+
+        <PrivateRoute exact path='/pokemons'>
+          <Pokemons />
+        </PrivateRoute>
+        <PrivateRoute exact path='/pokemons/:name'>
+          <PokemonsDetails />
+        </PrivateRoute>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/PokemonsDetail/PokemonsDetails.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
+function PokemonDetails() {
+    let { name } = useParams();
+    const [pokemon, setPokemon] = useState({});
+
+    function fetchSinglePokemon() {
+        let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((result) => setPokemon(result));
+    }
+
+    useEffect(() => {
+        fetchSinglePokemon();
+
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <div>
+            {pokemon.sprites !== undefined && (
+                <img src={pokemon.sprites.front_shiny} alt="pokemon" />
+            )}
+              <p>Name: {pokemon.name} </p>
+              <p>Weight:{pokemon.weight}</p>
+              <p>Height:{pokemon.height}</p>
+              <p>Experience :{pokemon.base_experience}</p>
+        </div>
+    );
+}
+
+export default PokemonDetails;
